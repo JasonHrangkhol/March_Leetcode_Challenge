a329bbfb33ae73bde240c7e808b7d7d179e3676f
Jason Hrangkhol <hrangkholjason@gmail.com>
2022-03-04 16:07:16 +0530

    Day 3 better solution

--- day3.cpp
+++ day3.cpp
@@ -2,37 +2,27 @@ class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
         
-        int n = nums.size(),ans=0;
-        int** dp = new int*[n];
-        for(int i=0;i<n;i++)
-            dp[i]= new int[n];
-        
-        for(int i=0;i<n;i++)
-            dp[i][i]=0;
-        for(int i=1;i<n;i++)
-            dp[i-1][i]=nums[i]-nums[i-1];
-        for(int i=0;i+2<n;i++){
-            int j = i+2;
-            if(nums[i+1]-nums[i] == nums[j]-nums[j-1]){
-                ans++;
-                dp[i][j] =nums[j]-nums[j-1];
-            }
+        int n = nums.size();  
+        if(n<3)
+            return 0;
+        int diff;
+        int ans  =0 ;     
+        diff = nums[1]-nums[0];
+        int start =0,end=1;
+        for(int i=2;i<n;i++)
+        {
+            if(diff==nums[i]-nums[i-1])
+                end=i;
             else
-                dp[i][j]=5000;
-        }        
-            
-        for(int len=4;len<=n;len++){
-            
-            for(int i=0;i+len-1<n;i++){
-                int j = i+len-1;
-                if(nums[i+1]-nums[i] == nums[j]-nums[j-1] && dp[i+1][j-1]==nums[i+1]-nums[i]){
-                    ans++;
-                    dp[i][j] = dp[i+1][j-1];
-                }
-                else
-                    dp[i][j]=5000;
+            {
+                start=i-1,end=i;
+                diff = nums[end]-nums[start];
             }
+            
+            if(end-start+1>=3)
+                ans=ans+(end-start-1);
         }
         return ans;
+        
     }
 };
